63ae9c12daa988325af1103359b1c68228a25a1f
Basil Suter <git@nadar.io>
2018-10-16 11:34:24 +0200

    order interface

--- src/base/OrderInterface.php
+++ src/base/OrderInterface.php
@@ -2,6 +2,11 @@
 
 namespace luya\payment\base;
 
+/**
+ * An interface you can attach to your estore order model.
+ * 
+ * This is just a recommendation for the payment process.
+ */
 interface OrderInterface
 {
     public function getId();
@@ -11,4 +16,8 @@ interface OrderInterface
     public function createToken();
 
     public function updatePayId($payId);
+
+    public function getPayId();
+
+    public function isDone();
 }
