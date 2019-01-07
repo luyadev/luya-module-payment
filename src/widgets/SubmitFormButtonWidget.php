ed540941ddfbbeec7c3bb5c842544fcc066846ce
Basil Suter <git@nadar.io>
2019-01-07 14:19:48 +0100

    add idea for submit button widget #9

---
+++ src/widgets/SubmitFormButtonWidget.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace luya\payment\widgets;
+
+use luya\base\Widget;
+use luya\helpers\Html;
+use luya\helpers\ArrayHelper;
+use yii\base\InvalidConfigException;
+
+/**
+ * Generates a submit button for a form. This should be used when submiting payment forms
+ * in order to ensure a request is not send twice.
+ * 
+ * ```php
+ * $form = ActiveForm::begin();
+ * // form code
+ * 
+ * SubmitFormButtonWidget::widget(['label' => 'Submit', 'pushed' => 'Sending ...', 'options' => ['class' => 'btn btn-primary']]);
+ * $form::end();
+ * ```
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ * @since 1.0.0
+ */
+class SubmitFormButtonWidget extends Widget
+{
+    /**
+     * @var string The label which should be displayed on button.
+     */
+    public $label;
+    
+    /**
+     * @var string The label which should be visible when the button is pushed. for example `... sending`.
+     */
+    public $pushed;
+
+    /**
+     * @var array An array with Options which can be passed to the button, see {{luya\helpers\Html::submitButton}}.
+     */
+    public $options = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (!$this->label) {
+            throw new InvalidConfigException("The label property can not be empty.");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function run()
+    {
+        $js = [
+            'this.disabled=true',
+        ];
+
+        if ($this->pushed) {
+            $js[] = "this.value='{$this->pushed}'";
+        }
+
+        return Html::submitButton($this->label, ArrayHelper::merge([
+            'onclick' => implode(" ", $js),
+        ], $this->options));
+    }
+}
