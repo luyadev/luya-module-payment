67fcf4a2a6a931b7e5a699d302dd3e6e6ec97678
Basil Suter <git@nadar.io>
2019-07-24 16:04:35 +0200

    new stripe sca payment flow with intents api #13

--- src/base/Provider.php
+++ src/base/Provider.php
@@ -22,6 +22,19 @@ abstract class Provider extends BaseObject implements ProviderInterface
      * Call a method of a the current object which is prefix with call and sanitize its variables to match action variables.
      *
      * Is currently used as its more readable for integrations as most methos have a lot of parameters.
+     * 
+     * ```php
+     * $provider->call('foo-bar', ['name' => 'john']);
+     * ```
+     * 
+     * would call
+     * 
+     * ```php
+     * public function callFooBar($name)
+     * {
+     *    return $name;
+     * }
+     * ```
      *
      * @param string $method The method to call without the `call` prefix.
      * @param array $vars Options to pass to the method.
--- src/providers/StripeProvider.php
+++ src/providers/StripeProvider.php
@@ -2,7 +2,11 @@
 
 namespace luya\payment\providers;
 
+
+use Exception;
+use Yii;
 use luya\payment\base\Provider;
+use Stripe\PaymentIntent;
 
 /**
  * Strip Provider
@@ -15,5 +19,67 @@ class StripeProvider extends Provider
     public function getId()
     {
         return 'stripe';
+    }
+
+    public function callGeneratePaymentMethodResponse($paymentMethodId, $totalAmount, $currency)
+    {
+        try {
+            $intent = PaymentIntent::create([
+                'payment_method' => $paymentMethodId,
+                'amount' => $totalAmount,
+                'currency' => $currency,
+                'confirmation_method' => 'manual',
+                'confirm' => true,
+            ]);
+        } catch (Exception $exception) {
+            return $this->exceptionResponse($exception);
+        }
+
+        return $this->handleIntentResponse($intent);
+    }
+
+    public function callGenerateIntentResponse($intentId)
+    {
+        try {
+            $intent = PaymentIntent::retrieve($intentId);
+            $intent->confirm();
+        } catch (Exception $exception) {
+            return $this->exceptionResponse($exception);
+        }
+
+        return $this->handleIntentResponse($intent);
+    }
+
+    public function callVerifySuccessIntent($id)
+    {
+        return PaymentIntent::retrieve($id)->status === PaymentIntent::STATUS_SUCCEEDED;
+    }
+
+    protected function handleIntentResponse(PaymentIntent $intent)
+    {
+        if ($intent->status == PaymentIntent::STATUS_REQUIRES_ACTION &&
+            $intent->next_action->type == 'use_stripe_sdk') {
+            # Tell the client to handle the action
+            return [
+                'requires_action' => true,
+                'payment_intent_client_secret' => $intent->client_secret
+            ];
+        } elseif ($intent->status == PaymentIntent::STATUS_SUCCEEDED) {
+            # The payment didn’t need any additional actions and completed!
+            # Handle post-payment fulfillment
+            return [
+                'success' => true,
+                'id' => $intent->id,
+            ];
+        } else {
+            Yii::$app->response->statusCode = 400;
+            return ['error' => 'Invalid PaymentIntent status', 'status' => $intent->status];
+        }
+    }
+
+    protected function exceptionResponse(Exception $exception)
+    {
+        Yii::$app->response->statusCode = 400;
+        return ['error' => ['message' => $exception->getMessage()]];
     }
 }
