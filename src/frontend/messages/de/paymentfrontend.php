6f6c2094f54c411f6fd7537ad39d788765f1ca30
Basil <git@nadar.io>
2020-02-17 21:53:36 +0100

    Update paymentfrontend.php

--- src/frontend/messages/de/paymentfrontend.php
+++ src/frontend/messages/de/paymentfrontend.php
@@ -4,7 +4,7 @@ return [
     'stripe_header_title' => 'Bezahlung',
     'stripe_item_list_title' => 'Details',
     'stripe_items_total' => 'Total',
-    'stripe_card_info' => 'Mit Kredit oder debit Karte bezahlen',
+    'stripe_card_info' => 'Mit Kredit- oder Debitkarte bezahlen',
     'stripe_button_abort' => 'Abbrechen',
     'stripe_button_pay' => 'Bezahlen',
 ];
