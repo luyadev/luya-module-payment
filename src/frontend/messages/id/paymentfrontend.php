1c5100f2260fd2d754fd9c614f9d02644f639d16
Dennis <dennisgon@users.noreply.github.com>
2023-10-31 13:34:27 +0700

    translate into bahasa (#30)

---
+++ src/frontend/messages/id/paymentfrontend.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'stripe_header_title' => 'Pembayaran Checkout',
+    'stripe_item_list_title' => 'Rincian',
+    'stripe_items_total' => 'Total',
+    'stripe_card_info' => 'Bayar dengan kartu kredit atau debit',
+    'stripe_button_abort' => 'Batal',
+    'stripe_button_pay' => 'Bayar',
+];
+
